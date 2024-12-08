9e52832006edb20b709f0d8e46cd4319ee548fa0
tiamiyu rasheed <tiamiyurasheed.tr@gmail.com>
2024-12-09 00:21:28 +0100

    fix navigation link

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+
 import './directory-item.styles.scss'
 
 const DirectoryItem = ({ category }) => {
-    const { title, imageUrl } = category;
+    const { title, imageUrl, route } = category;
+    const navigate = useNavigate();
+
+    const onNavigationHandler = () => navigate(route);
     return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigationHandler}>
         <div className='background-image' style={{ backgroundImage:`url(${imageUrl})` }} />
         <div className="body">
         <h2>{ title }</h2>
