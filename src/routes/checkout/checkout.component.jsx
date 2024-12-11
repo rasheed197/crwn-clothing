71460e91c5f0a3cc4f1f8316843f63f7bea1143b
tiamiyu rasheed <tiamiyurasheed.tr@gmail.com>
2024-12-11 02:17:52 +0100

    mobile responsiveness

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, Fragment } from 'react';
 
 import './checkout.styles.scss';
 import { CartContext } from '../../contexts/cart.context';
@@ -7,23 +7,27 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 const Checkout = () => {
     const { cartItems, cartTotal } = useContext(CartContext)
     return (
-        <div className="checkout-container">
-            <div className='checkout-header'>
-                <div className='header-block'><span>product</span></div>
-                <div className='header-block'><span>name</span></div>
-                <div className='header-block'><span>quantity</span></div>
-                <div className='header-block'><span>price</span></div>
-                <div className='header-block'><span>remove</span></div>
+        <Fragment>
+            <div className="checkout-container">
+                <div className='checkout-header'>
+                    <div className='header-block'><span>product</span></div>
+                    <div className='header-block'><span>name</span></div>
+                    <div className='header-block'><span>quantity</span></div>
+                    <div className='header-block'><span>price</span></div>
+                    <div className='header-block'><span>remove</span></div>
+                </div>
+                {
+                    cartItems.map((cartItem) => {
+                        return (
+                            <CheckoutItem key={cartItem.id} cartItem={ cartItem }/>
+                        )
+                    })
+                }
             </div>
-            {
-                cartItems.map((cartItem) => {
-                    return (
-                        <CheckoutItem key={cartItem.id} cartItem={ cartItem }/>
-                    )
-                })
-            }
-            <span className='total'>total: &#36;{ cartTotal }</span>
-        </div>
+            <div className='checkout-total-container'>
+                <span className='checkout-total'>total: &#36;{ cartTotal }</span>
+            </div>
+        </Fragment>
     )
 }
 
